8cc46e864245f9b5a87a82fdd609f7d9a569b141
mustafa.sacli <mustasacli@hotmail.com>
2022-08-13 02:08:41 +0300

    single named method added.

---
+++ Simply.TestApps.Soln/SimplyTest_Single_ConsoleApp/Program.cs
@@ -0,0 +1,141 @@
+﻿using MySql.Data.MySqlClient;
+using Simply.Common.Objects;
+using Simply.Data;
+using Simply.Data.Objects;
+using SimplyTest_Entities;
+using System;
+using System.Data;
+using System.Reflection;
+
+namespace SimplyTest_Single_ConsoleApp
+{
+    internal class Program
+    {
+        internal static IDbConnection GetDbConnection()
+        {
+            return new MySqlConnection { ConnectionString = "data source=127.0.0.1;initial catalog=classicmodels;user id=root;" };
+        }
+
+        private static void Main(string[] args)
+        {
+            int customerNumber = 103;
+            using (IDbConnection connection = GetDbConnection())
+            {
+                try
+                {
+                    connection.OpenIfNot();
+                    var orderDetail = connection.QuerySingle<Customers>("SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = ?customerNumber?",
+                        new { customerNumber }, parameterNamePrefix: '?');
+                    writeEntity(orderDetail);
+                }
+                finally
+                { connection.CloseIfNot(); }
+            }
+            Console.WriteLine("--------------------------------------------------------");
+            using (IDbConnection connection = GetDbConnection())
+            {
+                try
+                {
+                    connection.OpenIfNot();
+                    var customer = connection.GetSingle<Customers>(
+                        "SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = ?",
+                        new object[] { customerNumber });
+                    writeEntity(customer);
+                }
+                finally
+                { connection.CloseIfNot(); }
+            }
+            Console.WriteLine("--------------------------------------------------------");
+
+            using (IDbConnection connection = GetDbConnection())
+            {
+                try
+                {
+                    SimpleDbCommand command = new SimpleDbCommand();
+                    command.CommandText =
+                        "SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = @customerNumber";
+                    command.AddParameter(new DbCommandParameter { Direction = ParameterDirection.Input, ParameterName = "customerNumber", Value = customerNumber });
+                    connection.OpenIfNot();
+                    var customer = connection.QuerySingle<Customers>(command);
+                    writeEntity(customer.Result);
+                }
+                finally
+                { connection.CloseIfNot(); }
+            }
+            Console.WriteLine("--------------------------------------------------------");
+
+            using (IDbConnection connection = GetDbConnection())
+            {
+                try
+                {
+                    connection.OpenIfNot();
+                    var customerRow = connection.QuerySingleAsDbRow(
+                        "SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = @customerNumber",
+                        new { customerNumber });
+                    writeDbRow(customerRow);
+                }
+                finally
+                { connection.CloseIfNot(); }
+            }
+            Console.WriteLine("--------------------------------------------------------");
+            using (IDbConnection connection = GetDbConnection())
+            {
+                try
+                {
+                    connection.OpenIfNot();
+                    var customerRow = connection.QuerySingleAsDbRow(
+                        "SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = #customerNumber#",
+                        new { customerNumber }, parameterNamePrefix: '#');
+                    writeDbRow(customerRow);
+                }
+                finally
+                { connection.CloseIfNot(); }
+            }
+            Console.WriteLine("--------------------------------------------------------");
+            try
+            {
+                string productCode = "S18_2325";
+                using (IDbConnection connection = GetDbConnection())
+                {
+                    try
+                    {
+                        connection.OpenIfNot();
+                        var orderDetail = connection.QuerySingle<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
+                            new { productCode }, parameterNamePrefix: '?');
+                        writeEntity(orderDetail);
+                    }
+                    finally
+                    { connection.CloseIfNot(); }
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.ToString()); }
+            Console.WriteLine("--------------------------------------------------------");
+
+            Console.ReadKey();
+        }
+
+        private static void writeDbRow(SimpleDbRow row)
+        {
+            if (row != null)
+            {
+                foreach (SimpleDbCell cell in row.Cells)
+                {
+                    Console.WriteLine($"Name: {cell.CellName}, Type: {cell.CellType.Name}, Value:{cell.Value?.ToString() ?? "'NULL'"}");
+                }
+                Console.WriteLine("************************************");
+            }
+        }
+
+        private static void writeEntity<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity == null) return;
+
+            var properties = typeof(TEntity).GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine($"Property Name:#{property.Name}#, Type: #{property.PropertyType.Name}#, Value:#{property.GetValue(entity, null) ?? "'#NULL#'"}#");
+            }
+        }
+    }
+}
