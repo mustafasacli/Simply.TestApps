8be7b15306bc44d7d5649a56f6605cdb622236c5
mustafa.sacli <mustasacli@hotmail.com>
2022-08-14 18:06:26 +0300

    query arranged.

--- database/Mysql/Query1.sql
+++ database/Mysql/Query1.sql
@@ -11,4 +11,4 @@ SELECT EXT2.* FROM (SELECT EXT1.*, ROW_NUMBER() OVER() AS RN1 FROM ( #SQL_SCRIPT
 SELECT EXT2.* FROM (SELECT EXT1.*, ROW_NUMBER() OVER() AS RN1 FROM ( #SQL_SCRIPT# ) AS EXT1) AS EXT2 ORDER BY EXT2.RN1 LIMIT 0, 1
 select * from `classicmodels`.`orderdetails` WHERE `productCode` = 'S18_2325'
 
-orderdetailsSELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = 103
+SELECT * FROM `classicmodels`.`customers` WHERE `customerNumber` = 103
