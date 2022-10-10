bb33aa23573d4956e3a82b614dea33ac8d1bac38
mustafa sacli <mustasacli@hotmail.com>
2022-10-11 01:28:23 +0300

    dbrow list app arranged.

--- Simply.TestApps.Soln/SimplyTest_DbRowList_ConsoleApp/Program.cs
+++ Simply.TestApps.Soln/SimplyTest_DbRowList_ConsoleApp/Program.cs
@@ -1,102 +1,66 @@
-﻿using MySql.Data.MySqlClient;
-using Simply.Common;
+﻿using Simply.Common;
 using Simply.Common.Objects;
 using Simply.Data;
+using Simply.Data.Interfaces;
 using Simply.Data.Objects;
+using Simply_Test_Db;
 using System;
 using System.Collections.Generic;
-using System.Data;
 
 namespace SimplyTest_DbRowList_ConsoleApp
 {
     internal class Program
     {
-        internal static IDbConnection GetDbConnection()
-        {
-            return new MySqlConnection { ConnectionString = "data source=127.0.0.1;initial catalog=classicmodels;user id=root;" };
-        }
-        // SELECT * FROM orderdetails WHERE productCode='S18_2325';
-        // select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode;
-        // select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?;
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string productCode = "S18_2325";
+            ICommandSetting commandSetting = SimpleCommandSetting.Create(parameterNamePrefix: '?');
+            List<SimpleDbRow> rowList;
+            ISimpleDatabase database = new SimpleMySqlDatabase();
 
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryDbRowList("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
-                        new { productCode },
-                        commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'), pageInfo: null);
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
-            Console.WriteLine("--------------------------------------------------------");
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryDbRowList("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
-                        new { productCode });
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+            string sql = "select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?";
+            rowList = database.ListRow(sql, new { productCode }, commandSetting, pageInfo: null);
+            Console.WriteLine($"{rowList.Count} rows returned.");
+            writeDbRows(rowList);
             Console.WriteLine("--------------------------------------------------------");
 
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryDbRowList("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
-                        new { productCode }, commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?')
-                        , pageInfo: PageInfo.GetPageWithPageNumber(2, 10));
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+            sql = "select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode";
+            rowList = database.ListRow(sql, new { productCode }, pageInfo: null);
+            Console.WriteLine($"{rowList.Count} rows returned.");
+            writeDbRows(rowList);
             Console.WriteLine("--------------------------------------------------------");
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryDbRowList("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
-                        new { productCode }, pageInfo: PageInfo.GetPageWithPageNumber(3, 10));
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+
+            sql = "select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?";
+            rowList = database.ListRow(sql, new { productCode },
+                commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'),
+                pageInfo: PageInfo.GetPageWithPageNumber(2, 10));
+            Console.WriteLine($"{rowList.Count} rows returned.");
+            writeDbRows(rowList);
+            Console.WriteLine("--------------------------------------------------------");
+
+            sql = "select * from `classicmodels`.`orderdetails` WHERE `productCode` =  @productCode";
+            rowList = database.ListRow(sql, new { productCode },
+                commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'),
+                pageInfo: PageInfo.GetPageWithPageNumber(3, 10));
+            Console.WriteLine($"{rowList.Count} rows returned.");
+            writeDbRows(rowList);
             Console.WriteLine("--------------------------------------------------------");
 
             Console.ReadKey();
         }
 
-        static void writeDbRows(List<SimpleDbRow> rows)
+        private static void writeDbRows(List<SimpleDbRow> rows)
         {
-            if (!ArrayHelper.IsNullOrEmpty(rows))
+            if (ArrayHelper.IsNullOrEmpty(rows))
+                return;
+
+            foreach (SimpleDbRow row in rows)
             {
-                foreach (SimpleDbRow row in rows)
+                foreach (SimpleDbCell cell in row.Cells)
                 {
-                    foreach (SimpleDbCell cell in row.Cells)
-                    {
-                        Console.WriteLine($"Name: {cell.CellName}, Type: {cell.CellType.Name}, Value:{cell.Value?.ToString() ?? "'NULL'"}");
-                    }
-                    Console.WriteLine("************************************");
+                    Console.WriteLine($"Name: {cell.CellName}, Type: {cell.CellType.Name}, Value:{cell.Value?.ToString() ?? "'NULL'"}");
                 }
+                Console.WriteLine("************************************");
             }
         }
     }
