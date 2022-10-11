c8abe2389fb3cb30a4811db3dd33029d0cbd38cc
mustafa sacli <mustasacli@hotmail.com>
2022-10-11 22:00:38 +0300

    list app developments.

--- Simply.TestApps.Soln/SimplyTest_List_ConsoleApp/Program.cs
+++ Simply.TestApps.Soln/SimplyTest_List_ConsoleApp/Program.cs
@@ -1,79 +1,41 @@
-﻿using MySql.Data.MySqlClient;
-using Simply.Data;
+﻿using Simply.Data;
+using Simply.Data.Interfaces;
 using Simply.Data.Objects;
+using Simply_Test_Db;
 using SimplyTest_Entities;
 using System;
-using System.Data;
 
 namespace SimplyTest_List_ConsoleApp
 {
     internal class Program
     {
-        internal static IDbConnection GetDbConnection()
-        {
-            return new MySqlConnection { ConnectionString = "data source=127.0.0.1;initial catalog=classicmodels;user id=root;" };
-        }
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string productCode = "S18_2325";
 
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryList<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
-                        new { productCode }, commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'), pageInfo: null);
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    //writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
-            Console.WriteLine("--------------------------------------------------------");
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryList<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
-                        new { productCode });
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    //writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+            ISimpleDatabase database = new SimpleMySqlDatabase();
+            var rowList1 = database.List<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
+                new { productCode }, commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'), pageInfo: null);
+            Console.WriteLine($"{rowList1.Count} rows returned.");
+            //writeDbRows(rowList1);
             Console.WriteLine("--------------------------------------------------------");
 
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryList<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
-                        new { productCode }, commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'), pageInfo: PageInfo.GetPageWithPageNumber(2, 10));
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    //writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+            var rowList2 = database.List<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
+                new { productCode });
+            Console.WriteLine($"{rowList2.Count} rows returned.");
+            //writeDbRows(rowList2);
             Console.WriteLine("--------------------------------------------------------");
-            using (IDbConnection connection = GetDbConnection())
-            {
-                try
-                {
-                    connection.OpenIfNot();
-                    var rowList = connection.QueryList<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
-                        new { productCode }, pageInfo: PageInfo.GetPageWithPageNumber(3, 10));
-                    Console.WriteLine($"{rowList.Count} rows returned.");
-                    //writeDbRows(rowList);
-                }
-                finally
-                { connection.CloseIfNot(); }
-            }
+
+            var rowList3 = database.List<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = ?productCode?",
+                new { productCode }, commandSetting: SimpleCommandSetting.Create().SetParameterNamePrefix('?'), pageInfo: PageInfo.GetPageWithPageNumber(2, 10));
+            Console.WriteLine($"{rowList3.Count} rows returned.");
+            //writeDbRows(rowList3);
+            Console.WriteLine("--------------------------------------------------------");
+
+            var rowList4 = database.List<Orderdetails>("select * from `classicmodels`.`orderdetails` WHERE `productCode` = @productCode",
+                new { productCode }, pageInfo: PageInfo.GetPageWithPageNumber(3, 10));
+            Console.WriteLine($"{rowList4.Count} rows returned.");
+            //writeDbRows(rowList4);
             Console.WriteLine("--------------------------------------------------------");
 
             Console.ReadKey();
